9b09c7f3ab8e5a54acac2b4da2fd6f881344d9e8
Meghna Srivastava <meghna.s1906@gmail.com>
2018-06-03 12:35:26 +0200

    render queried data by source plugins

--- querying-in-blog/gatsby-config.js
+++ querying-in-blog/gatsby-config.js
@@ -1,11 +1,21 @@
 module.exports = {
-    plugins: [
-      `gatsby-plugin-glamor`,
-      {
-        resolve: `gatsby-plugin-typography`,
-        options: {
-          pathToConfigModule: `src/utils/typography`,
-        },
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
       },
-    ],
-  };
+    },
+    `gatsby-plugin-glamor`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+};
